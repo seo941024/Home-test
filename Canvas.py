046df62068a604deb85871822c0d28ec04aa8a66
Seo ji-seop <seo941024@gmail.com>
2026-03-15 23:53:11 +0900

    Home

---
+++ Canvas.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.title("Canvas")
+root.geometry("640x400+0+0")
+root.resizable(True, True)
+
+canvas = Canvas(root, width=200, height=150, bg="white", bd=2)
+canvas.pack(fill="both", expand=True) #
+
+canvas.create_line(0, int(200/2), 100, int(200/2), fill="blue")
+
+root.mainloop()
